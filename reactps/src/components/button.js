9c1857cd44ef7c741bec3f20cfe32e157851b4e7
Steven Hanson <essayhanson@gmail.com>
2019-02-09 18:23:58 +0000

    prevState added to handleClick function for button

--- reactps/src/components/button.js
+++ reactps/src/components/button.js
@@ -2,16 +2,14 @@ import React from 'react';
 
 class Button extends React.Component {
 
-    constructor(props) {
-
-        super(props);
-        this.state = { counter: 0 };
-    }
+    state = { counter: 0 };
     
     handleClick = () => {
-        this.setState({
-            counter: this.state.counter + 1
-        })
+        this.setState((prevState) => {
+            return {
+                counter: prevState.counter + 1
+            }
+        });
     }
 
     render() {
